eb78b0638beaea1c8e9ca3d1bd62470ebf0ca137
Trish Z <trishzag@gmail.com>
2015-10-04 18:59:16 -0400

    fixed spec_helper.rb error

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -5,7 +5,6 @@ Coveralls.wear!('rails')
 RSpec.configure do |config|
   config.expect_with :rspec do |expectations|
     expectations.include_chain_clauses_in_custom_matcher_descriptions = true
-    config.include LoginHelper
   end
 
   config.mock_with :rspec do |mocks|
