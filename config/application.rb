ae00f446e09a504dc7e81fff1ec6f59d62621a1a
Trish Z <trishzag@gmail.com>
2015-10-04 18:51:28 -0400

    fix hound error

--- config/application.rb
+++ config/application.rb
@@ -6,17 +6,6 @@ require 'rails/all'
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
-config.generators do |g|
-  g.test_framework :rspec,
-    fixtures: true,
-    view_specs: false,
-    helper_specs: false,
-    routing_specs: false,
-    controller_specs: true,
-    request_specs: true
-  g.fixture_replacement :factory_girl, dir: "spec/factories"
-end
-
 module KidCoders
   class Application < Rails::Application
     # Settings in config/environments/* take precedence over those specified here.
@@ -33,5 +22,16 @@ module KidCoders
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
+
+    config.generators do |g|
+      g.test_framework :rspec,
+        fixtures: true,
+        view_specs: false,
+        helper_specs: false,
+        routing_specs: false,
+        controller_specs: true,
+        request_specs: true
+      g.fixture_replacement :factory_girl, dir: "spec/factories"
+    end
   end
 end
