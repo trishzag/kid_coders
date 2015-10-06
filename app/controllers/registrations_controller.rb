af8e0a9e35bcd80559dd4202dbd51ea6a9554d72
Trish Z <trishzag@gmail.com>
2015-10-05 23:35:43 -0400

    added logo and modified views/css

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -4,4 +4,6 @@ class RegistrationsController < Devise::RegistrationsController
   def after_update_path_for(resource)
     user_path(resource)
   end
+
+  
 end
--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -5,11 +5,13 @@ before_filter :configure_account_update_params, only: [:update]
   protected
 
   def configure_sign_up_params
-    devise_parameter_sanitizer.for(:sign_up) << :username
+    devise_parameter_sanitizer.for(:sign_up) << :username, :first_name,
+    :last_name
   end
 
   def configure_account_update_params
-    devise_parameter_sanitizer.for(:account_update) << :username
+    devise_parameter_sanitizer.for(:account_update) << :username, :first_name,
+    :last_name
   end
 
   def after_sign_up_path_for(resource)
