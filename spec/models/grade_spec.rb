5f249344c9c62b92a0a9eadf81ad079bbd337a08
Trish Z <trishzag@gmail.com>
2015-10-06 23:53:44 -0400

    fixed hound error

--- spec/models/grade_spec.rb
+++ spec/models/grade_spec.rb
@@ -7,6 +7,6 @@ RSpec.describe Grade, type: :model do
   it { should_not have_valid(:name).when(nil, "") }
 
   it { should validate_presence_of(:user_id) }
-  
+
   it { should validate_presence_of(:assignment_id) }
 end
