7163bd64f7f14e44ebf6901383a8c5bdd910823c
Trish Z <trishzag@gmail.com>
2015-10-08 16:46:02 -0400

    added account deletion functionality

--- spec/features/users/user_deletes_account_spec.rb
+++ spec/features/users/user_deletes_account_spec.rb
@@ -1,9 +1,35 @@
-# As a User
-# I want to delete my account
-# So that my information is no longer available
-#
-# Acceptance Criteria
-# [ ] I must be signed in to delete my account
-# [ ] I may only delete my own account
-# [ ] I must be able to access the delete function from my user detail page
-# [ ] I must receive a confirmation when my account is deleted
+require 'rails_helper'
+
+feature 'user deletes account', %{
+  As a User
+  I want to delete my account
+  So that my information is no longer available
+
+  Acceptance Criteria
+  [X] I must be signed in to delete my account
+  [X] I may only delete my own account
+  [X] I must be able to access the delete function from my user detail page
+  [X] I must receive a confirmation when my account is deleted
+} do
+  let(:user) { FactoryGirl.create(:user) }
+
+  scenario 'user deletes the account' do
+    login_with_email(user)
+    click_link 'My Profile'
+    click_on 'Account Settings'
+    click_button 'Cancel my account'
+
+    expect(page).to have_content('account has been successfully cancelled.')
+    login_with_email(user)
+    click_button 'Log in'
+    expect(page).to have_content('Invalid login or password.')
+  end
+
+  scenario 'user cannot access users#delete if not authenticated' do
+    login_with_email(user)
+    click_link 'Sign Out'
+    visit edit_user_registration_path
+
+    expect(page).to have_content('You need to sign in or sign up before continuing.')
+  end
+end
