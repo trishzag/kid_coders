f2848ad8afd3be8256dc0acd10e77e383fba0b19
Trish Z <trishzag@gmail.com>
2015-10-08 16:15:35 -0400

    fixed hound errors

--- app/models/user.rb
+++ app/models/user.rb
@@ -38,8 +38,8 @@ class User < ActiveRecord::Base
     conditions = warden_conditions.dup
     if login = conditions.delete(:login)
       where(conditions.to_hash).where(
-      ["lower(username) = :value OR lower(email) = :value", { value: login.downcase }])
-      .first
+        ["lower(username) = :value OR lower(email) = :value",
+          { value: login.downcase }]).first
     else
       where(conditions.to_hash).first
     end
