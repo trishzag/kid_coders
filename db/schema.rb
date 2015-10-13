27e89388ddd092871ade6bbb7f4cb68ce703e998
Trish Z <trishzag@gmail.com>
2015-10-13 16:07:22 -0400

    installed merit to add badges

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151012010544) do
+ActiveRecord::Schema.define(version: 20151013150443) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -24,6 +24,17 @@ ActiveRecord::Schema.define(version: 20151012010544) do
   end
 
   add_index "assignments", ["title", "curriculum_id"], name: "index_assignments_on_title_and_curriculum_id", unique: true, using: :btree
+
+  create_table "badges_sashes", force: :cascade do |t|
+    t.integer  "badge_id"
+    t.integer  "sash_id"
+    t.boolean  "notified_user", default: false
+    t.datetime "created_at"
+  end
+
+  add_index "badges_sashes", ["badge_id", "sash_id"], name: "index_badges_sashes_on_badge_id_and_sash_id", using: :btree
+  add_index "badges_sashes", ["badge_id"], name: "index_badges_sashes_on_badge_id", using: :btree
+  add_index "badges_sashes", ["sash_id"], name: "index_badges_sashes_on_sash_id", using: :btree
 
   create_table "contents", force: :cascade do |t|
     t.string   "title",         null: false
@@ -62,12 +73,50 @@ ActiveRecord::Schema.define(version: 20151012010544) do
 
   add_index "groups", ["name", "id"], name: "index_groups_on_name_and_id", unique: true, using: :btree
 
+  create_table "merit_actions", force: :cascade do |t|
+    t.integer  "user_id"
+    t.string   "action_method"
+    t.integer  "action_value"
+    t.boolean  "had_errors",    default: false
+    t.string   "target_model"
+    t.integer  "target_id"
+    t.text     "target_data"
+    t.boolean  "processed",     default: false
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "merit_activity_logs", force: :cascade do |t|
+    t.integer  "action_id"
+    t.string   "related_change_type"
+    t.integer  "related_change_id"
+    t.string   "description"
+    t.datetime "created_at"
+  end
+
+  create_table "merit_score_points", force: :cascade do |t|
+    t.integer  "score_id"
+    t.integer  "num_points", default: 0
+    t.string   "log"
+    t.datetime "created_at"
+  end
+
+  create_table "merit_scores", force: :cascade do |t|
+    t.integer "sash_id"
+    t.string  "category", default: "default"
+  end
+
   create_table "resources", force: :cascade do |t|
     t.string   "name",          null: false
     t.string   "source",        null: false
     t.integer  "assignment_id"
     t.datetime "created_at",    null: false
     t.datetime "updated_at",    null: false
+  end
+
+  create_table "sashes", force: :cascade do |t|
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
   create_table "userplans", force: :cascade do |t|
@@ -97,6 +146,8 @@ ActiveRecord::Schema.define(version: 20151012010544) do
     t.datetime "updated_at",                             null: false
     t.integer  "group_id"
     t.boolean  "admin",                  default: false
+    t.integer  "sash_id"
+    t.integer  "level",                  default: 0
   end
 
   add_index "users", ["group_id"], name: "index_users_on_group_id", using: :btree
