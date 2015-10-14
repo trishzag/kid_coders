dcc549d599f9974e79d09116964946797f29bc1c
Trish Z <trishzag@gmail.com>
2015-10-14 11:26:43 -0400

    temp remove module Merit - heroku crash

--- app/models/merit/badge_rules.rb
+++ app/models/merit/badge_rules.rb
@@ -1,26 +1,26 @@
-module Merit
-  class BadgeRules
-    include Merit::BadgeRulesMethods
-    def initialize
-      grant_on 'assignments#complete', badge: 'dedicated-student',
-      to: :user do |assignment|
-      assignments.complete.count == 1
-      end
-
-      grant_on 'assignments#complete', badge: 'dedicated-learner',
-      to: :user do |assignment|
-      assignments.complete.count == 1
-      end
-
-      grant_on 'curriculum#complete', badge: 'coding-wizard',
-      to: :user do |curriculum|
-      curriculum.complete.count == 1
-      end
-
-      grant_on 'curriculum#complete', badge: 'programming-wizard',
-      to: :user do |curriculum|
-      curriculum.complete.count == 1
-      end
-    end
-  end
-end
+# module Merit
+#   class BadgeRules
+#     include Merit::BadgeRulesMethods
+#     def initialize
+#       grant_on 'assignments#complete', badge: 'dedicated-student',
+#       to: :user do |assignment|
+#       assignments.complete.count == 1
+#       end
+#
+#       grant_on 'assignments#complete', badge: 'dedicated-learner',
+#       to: :user do |assignment|
+#       assignments.complete.count == 1
+#       end
+#
+#       grant_on 'curriculum#complete', badge: 'coding-wizard',
+#       to: :user do |curriculum|
+#       curriculum.complete.count == 1
+#       end
+#
+#       grant_on 'curriculum#complete', badge: 'programming-wizard',
+#       to: :user do |curriculum|
+#       curriculum.complete.count == 1
+#       end
+#     end
+#   end
+# end
